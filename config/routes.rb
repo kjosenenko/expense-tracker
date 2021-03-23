9e592549fd732c406602d6a090d5e78f8684f517
KJ Osenenko <karljosenenko@gmail.com>
2021-03-23 11:14:39 -0600

    refractored users controller to create new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :transaction_types, only: [:index]
-  resources :users, only: [:index] do
+  resources :users, only: [:index, :create] do
     resources :transactions
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
